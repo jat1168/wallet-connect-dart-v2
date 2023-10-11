20678ba1bb76c74980fe4350fd4466000134e79d
jat1168 <jat1168@gmail.com>
2023-10-11 16:44:50 +0800

    fix error map empty issue

--- lib/wc_utils/jsonrpc/utils/validator.dart
+++ lib/wc_utils/jsonrpc/utils/validator.dart
@@ -1,30 +1,21 @@
 import 'package:wallet_connect_dart_v2/wc_utils/jsonrpc/models/models.dart';
 
 bool isJsonRpcPayload(dynamic payload) {
-  return (payload is Map &&
-      payload.containsKey('id') &&
-      payload.containsKey('jsonrpc') &&
-      payload['jsonrpc'] == '2.0');
+  return (payload is Map && payload.containsKey('id') && payload.containsKey('jsonrpc') && payload['jsonrpc'] == '2.0');
 }
 
 bool isJsonRpcRequest(dynamic payload) {
-  return payload is JsonRpcRequest ||
-      (payload is Map &&
-          isJsonRpcPayload(payload) &&
-          payload.containsKey('method'));
+  return payload is JsonRpcRequest || (payload is Map && isJsonRpcPayload(payload) && payload.containsKey('method'));
 }
 
 bool isJsonRpcResponse(dynamic payload) {
-  return isJsonRpcPayload(payload) &&
-      (isJsonRpcResult(payload) || isJsonRpcError(payload));
+  return isJsonRpcPayload(payload) && (isJsonRpcResult(payload) || isJsonRpcError(payload));
 }
 
 bool isJsonRpcResult(dynamic payload) {
-  return payload is JsonRpcResult ||
-      (payload is Map && payload.containsKey('result'));
+  return payload is JsonRpcResult || (payload is Map && payload.containsKey('result'));
 }
 
 bool isJsonRpcError(dynamic payload) {
-  return payload is JsonRpcError ||
-      payload is Map && payload.containsKey('error');
+  return payload is JsonRpcError || payload is Map && payload.containsKey('error') && payload.values.isNotEmpty;
 }
